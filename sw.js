cf95a174126fea350f340add44234c629029c73a
amruthwo <amruthwo@gmail.com>
2026-02-14 19:16:46 -0700

    Update sw.js
    v9

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "violin-falling-notes-v8";
+const CACHE = "violin-falling-notes-v9";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=8",
-  "./app.js?v=8",
+  "./style.css?v=9",
+  "./app.js?v=9",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
